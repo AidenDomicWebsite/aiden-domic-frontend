2a057b3f277d8269d8e4e187943713bac3d53eb3
mastra90 <115628366+mastra90@users.noreply.github.com>
2024-04-04 16:47:46 +1100

    Add functionality to the nav icon with animations

--- src/App.js
+++ src/App.js
@@ -1,31 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
 import './App.css';
 import ContactPage from './Pages/ContactPage';
 import HomePage from './Pages/HomePage';
 import ServicesPage from './Pages/ServicesPage';
-import NavIcon from './Assets/Icons/NavIcon';
-import XIcon from './Assets/Icons/XIcon';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faChevronRight, faX } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   return (
     <Router>
-    <div>
-    <header>
-    <NavIcon/>
-    <XIcon/>
-      <Link id='home'to="/">STRENTH TO BREATH</Link>
-        <nav>
-            <Link to="/services">SERVICES</Link>
-            <Link to="/contact">CONTACT</Link>
-        </nav>
-      </header>
-      <Routes>
+      <div>
+        <header>
+          <FontAwesomeIcon className={menuOpen ? 'x-icon' : 'x-icon-hide'} icon={faX} onClick={toggleMenu} />
+          <FontAwesomeIcon className='nav-mobile-icon' icon={faBars} onClick={toggleMenu} />
+          <Link id='home' onClick={closeMenu} to="/">STRENTH TO BREATH</Link>
+          <nav className={menuOpen ? 'menu-open' : ''}>
+          <Link to="/services" onClick={closeMenu}>SERVICES <FontAwesomeIcon className='chevron' icon={faChevronRight} /></Link>
+          <Link to="/contact" onClick={closeMenu}>CONTACT <FontAwesomeIcon className='chevron' icon={faChevronRight} /></Link>
+          </nav>
+        </header>
+        <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/contact" element={<ContactPage />} />
           <Route path="/services" element={<ServicesPage />} />
         </Routes>
-    </div>
+        {menuOpen && <div className="overlay" onClick={toggleMenu}></div>}
+      </div>
     </Router>
   );
 }
