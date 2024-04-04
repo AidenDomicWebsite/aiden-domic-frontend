631bcd92bf28377c12eb5c69cfe147b9509ca3d1
mastra90 <115628366+mastra90@users.noreply.github.com>
2024-04-04 14:29:33 +1100

    Configured and styled the navigation menu. The next task is to give the nav icon functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
+import React from 'react';
+import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
 import './App.css';
+import ContactPage from './Pages/ContactPage';
+import HomePage from './Pages/HomePage';
+import ServicesPage from './Pages/ServicesPage';
+import NavIcon from './Assets/Icons/NavIcon';
+import XIcon from './Assets/Icons/XIcon';
 
 function App() {
   return (
+    <Router>
     <div>
-      <h1>Hello</h1>
-      <h2>Hello</h2>
-      <p>Hello</p>
+    <header>
+    <NavIcon/>
+    <XIcon/>
+      <Link id='home'to="/">STRENTH TO BREATH</Link>
+        <nav>
+            <Link to="/services">SERVICES</Link>
+            <Link to="/contact">CONTACT</Link>
+        </nav>
+      </header>
+      <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/services" element={<ServicesPage />} />
+        </Routes>
     </div>
+    </Router>
   );
 }
 
