aff0f7723c1b7515f11cf7a1adcb51f2151609a9
mastra90 <115628366+mastra90@users.noreply.github.com>
2024-04-10 13:40:09 +1000

    Added content to the Contact page

--- src/Pages/ContactPage.js
+++ src/Pages/ContactPage.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import ContactFormCard from '../Components/ContactFormCard'
 
 const ContactPage = () => {
     return (
-      <div>
-        <h1>Contact page</h1>
+      <div className='contact-page-container'>
+        <div className='contact-details'>
+          <h1 className='our-details-heading'>OUR DETAILS</h1>
+          <a className='footerhome' href="/" >STRENGTH TO BREATH</a>
+          <p >50/100 example st, Melbourne</p>
+          <p >example@email.com</p>
+          <p >0412 345 678</p>
+        </div>
+        <div className='contact-form-container'>
+          < ContactFormCard/>
+        </div>
       </div>
     );
   };
