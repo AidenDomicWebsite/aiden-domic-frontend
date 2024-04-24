2aa8c4c5c893f828385e1796185e085034a0539f
mastra90 <115628366+mastra90@users.noreply.github.com>
2024-04-24 12:40:13 +1000

    Sample video on homescreen now autoplays with no audio

--- src/Components/PromoVideo.jsx
+++ src/Components/PromoVideo.jsx
@@ -4,10 +4,8 @@ import promoVideo from '../Assets/Promo_video.mp4';
 const PromoVideo = () => {
   return (
     <div>
-        <div className='video-container'>
-            <video controls>
-                <source src={promoVideo} type="video/mp4" />
-                Your browser does not support the video tag.
+        <div>
+            <video src={promoVideo} autoPlay loop muted>
             </video>
         </div>
     </div>
